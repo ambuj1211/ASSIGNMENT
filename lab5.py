f0e475dc5dd10066112b272cd3b902272ff03912
ambuj1211 <brilliantambuj5@gmail.com>
2023-11-20 19:19:12 +0530

    question number 5

--- lab5.py
+++ lab5.py
@@ -4,3 +4,14 @@ b = int(input("Give the base of parallelogram: "))
 h = int(input("Give the height of parallelogram: "))
 for i in range(h):
     print("*"*b)
+
+''' 
+Give the base of parallelogram: 8
+Give the height of parallelogram: 5
+********
+********
+********
+********
+********
+
+'''
