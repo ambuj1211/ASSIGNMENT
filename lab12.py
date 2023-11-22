f8af62b5f6fcb241718191640f7072dd5ed2df55
ambuj1211 <brilliantambuj5@gmail.com>
2023-11-22 14:42:28 +0530

    Que.NO. 12

--- lab12.py
+++ lab12.py
@@ -1,7 +1,28 @@
+def binary_search(arr, x):
+    low = 0
+    high = len(arr) - 1
+    mid = 0
+ 
+    while low <= high:
+ 
+        mid = (high + low) // 2
+        if arr[mid] < x:
+            low = mid + 1
+        elif arr[mid] > x:
+            high = mid - 1
+        else:
+            return mid
+    return -1
+ 
 n = int(input("Enter number of planets: "))
 lst = [int(i) for i in input("List of distances of planet: ").split()]
 k = int(input("position of Aman's favourite planet in unsorted list: "))
-print(lst[k-1])
+tem = lst[k-1]
+
+lst.sort()
+result = binary_search(lst, tem)
+print("Element is present at index: ", result+1)
+
 
 '''
 output
@@ -9,5 +30,5 @@ output
 Enter number of planets: 5
 List of distances of planet: 4 5 3 1 2
 position of Aman's favourite planet in unsorted list: 2
-5
+Element is present at index:  5
 '''
