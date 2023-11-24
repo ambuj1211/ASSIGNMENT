cdb2164dd498fb62bf15e2d64640195d6901aadf
ambuj1211 <brilliantambuj5@gmail.com>
2023-11-24 09:02:44 +0530

    Ques.no.15

--- lab15.py
+++ lab15.py
@@ -3,15 +3,17 @@ def DiagCalc(matrix):
         print("Input matrix is not square.")
         return
     
-left_diag_sum = 0
-right_diag_sum = 0
+    left_diag_sum = 0
+    right_diag_sum = 0
 
-for i in range(len(matrix)):
-    left_diag_sum += matrix[i][i] 
-    right_diag_sum += matrix[i][len(matrix) - 1 - i] 
+    for i in range(len(matrix)):
+        left_diag_sum += matrix[i][i] 
+        right_diag_sum += matrix[i][len(matrix) - 1 - i] 
 
-print("Sum of left diagonal:", left_diag_sum)
-print("Sum of right diagonal:", right_diag_sum)
-
-matrix = [[1, 2, 3], [3, 4, 5], [6, 7, 8]]
+    print("Sum of left diagonal:", left_diag_sum)
+    print("Sum of right diagonal:", right_diag_sum)
+    
+c = int(input("Enter number of columns: "))
+r = int(input("Enter number of rows: "))
+matrix = [[int(input()) for x in range (c)] for y in range(r)]
 DiagCalc(matrix)
